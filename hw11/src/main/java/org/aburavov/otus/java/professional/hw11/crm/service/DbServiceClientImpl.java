40ac9ba4b4f75ab49d7e6acf1a7950f545200c4d
Alexey Buravov <aburavov189@gmail.com>
2026-06-25 04:17:26 +0500

    HW11

--- hw11/src/main/java/org/aburavov/otus/java/professional/hw11/crm/service/DbServiceClientImpl.java
+++ hw11/src/main/java/org/aburavov/otus/java/professional/hw11/crm/service/DbServiceClientImpl.java
@@ -1,5 +1,7 @@
 package org.aburavov.otus.java.professional.hw11.crm.service;
 
+import org.aburavov.otus.java.professional.hw11.cachehw.HwListener;
+import org.aburavov.otus.java.professional.hw11.cachehw.MyCache;
 import org.aburavov.otus.java.professional.hw11.core.repository.DataTemplate;
 import org.aburavov.otus.java.professional.hw11.core.sessionmanager.TransactionManager;
 import org.aburavov.otus.java.professional.hw11.crm.model.Client;
@@ -13,11 +15,22 @@ public class DbServiceClientImpl implements DBServiceClient {
     private static final Logger log = LoggerFactory.getLogger(DbServiceClientImpl.class);
 
     private final DataTemplate<Client> clientDataTemplate;
+    private final MyCache<Long, Client> cache;
     private final TransactionManager transactionManager;
 
     public DbServiceClientImpl(TransactionManager transactionManager, DataTemplate<Client> clientDataTemplate) {
         this.transactionManager = transactionManager;
         this.clientDataTemplate = clientDataTemplate;
+        this.cache = new MyCache<>();
+
+        HwListener<Long, Client> listener = new HwListener<Long, Client>() {
+            @Override
+            public void notify(Long key, Client value, String action) {
+                log.info("Cache: key:{}, action: {}", key, action);
+            }
+        };
+
+        cache.addListener(listener);
     }
 
     @Override
@@ -27,19 +40,27 @@ public class DbServiceClientImpl implements DBServiceClient {
             if (client.getId() == null) {
                 var savedClient = clientDataTemplate.insert(session, clientCloned);
                 log.info("created client: {}", clientCloned);
+                cache.remove(savedClient.getId());
                 return savedClient;
             }
             var savedClient = clientDataTemplate.update(session, clientCloned);
             log.info("updated client: {}", savedClient);
+            cache.remove(savedClient.getId());
             return savedClient;
         });
     }
 
     @Override
     public Optional<Client> getClient(long id) {
+        Client cachedClient = cache.get(id);
+        if (cachedClient != null) {
+            return Optional.of(cachedClient);
+        }
+
         return transactionManager.doInReadOnlyTransaction(session -> {
             var clientOptional = clientDataTemplate.findById(session, id);
             log.info("client: {}", clientOptional);
+            clientOptional.ifPresent(client -> cache.put(id, client));
             return clientOptional;
         });
     }
