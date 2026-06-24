38f343bdabad12a6cfb7f35630afd92f87931513
Alexey Buravov <aburavov189@gmail.com>
2026-06-24 05:20:59 +0500

    HW10

--- hw10/src/main/java/org/aburavov/otus/java/professional/hw10/DbServiceDemo.java
+++ hw10/src/main/java/org/aburavov/otus/java/professional/hw10/DbServiceDemo.java
@@ -1,14 +1,16 @@
 package org.aburavov.otus.java.professional.hw10;
 
-import org.hibernate.cfg.Configuration;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.util.List;
 import org.aburavov.otus.java.professional.hw10.core.repository.DataTemplateHibernate;
 import org.aburavov.otus.java.professional.hw10.core.repository.HibernateUtils;
 import org.aburavov.otus.java.professional.hw10.core.sessionmanager.TransactionManagerHibernate;
-import org.aburavov.otus.java.professional.hw10.crm.dbmigrations.MigrationsExecutorFlyway;
+import org.aburavov.otus.java.professional.hw10.crm.model.Address;
 import org.aburavov.otus.java.professional.hw10.crm.model.Client;
+import org.aburavov.otus.java.professional.hw10.crm.model.Phone;
 import org.aburavov.otus.java.professional.hw10.crm.service.DbServiceClientImpl;
+import org.hibernate.cfg.Configuration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class DbServiceDemo {
 
@@ -19,27 +21,31 @@ public class DbServiceDemo {
     public static void main(String[] args) {
         var configuration = new Configuration().configure(HIBERNATE_CFG_FILE);
 
-        var dbUrl = configuration.getProperty("hibernate.connection.url");
-        var dbUserName = configuration.getProperty("hibernate.connection.username");
-        var dbPassword = configuration.getProperty("hibernate.connection.password");
-
-        new MigrationsExecutorFlyway(dbUrl, dbUserName, dbPassword).executeMigrations();
-
-        var sessionFactory = HibernateUtils.buildSessionFactory(configuration, Client.class);
+        var sessionFactory = HibernateUtils.buildSessionFactory(configuration, Client.class, Address.class, Phone.class);
 
         var transactionManager = new TransactionManagerHibernate(sessionFactory);
-        ///
         var clientTemplate = new DataTemplateHibernate<>(Client.class);
-        ///
         var dbServiceClient = new DbServiceClientImpl(transactionManager, clientTemplate);
-        dbServiceClient.saveClient(new Client("dbServiceFirst"));
 
-        var clientSecond = dbServiceClient.saveClient(new Client("dbServiceSecond"));
+        var firstClient = new Client(
+                null,
+                "Ivan",
+                new Address("Pushkina"),
+                List.of(new Phone("101"), new Phone("102")));
+        dbServiceClient.saveClient(firstClient);
+
+
+        var clientSecond = dbServiceClient.saveClient(new Client(
+                null,
+                "Alexander",
+                new Address("Abaya"),
+                List.of(new Phone("201"))));
+
         var clientSecondSelected = dbServiceClient
                 .getClient(clientSecond.getId())
                 .orElseThrow(() -> new RuntimeException("Client not found, id:" + clientSecond.getId()));
         log.info("clientSecondSelected:{}", clientSecondSelected);
-        ///
+
         dbServiceClient.saveClient(new Client(clientSecondSelected.getId(), "dbServiceSecondUpdated"));
         var clientUpdated = dbServiceClient
                 .getClient(clientSecondSelected.getId())
